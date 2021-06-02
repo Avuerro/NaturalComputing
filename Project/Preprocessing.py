3df894a27120cf7dd86917ed59a4f2ef8f62846c
MaxDriessen <driessenmax@gmail.com>
2021-06-03 00:25:12 +0200

    Added DEAPWANN and updated preprocessing

--- Project/Preprocessing.py
+++ Project/Preprocessing.py
@@ -7,7 +7,7 @@ from tensorflow.keras.datasets import mnist, fashion_mnist
 from skimage.transform import resize
 
 # Standard python imports
-import argparse, pickle, os
+import argparse, pickle, os, cv2
 import numpy as np
 
 
@@ -19,13 +19,9 @@ import numpy as np
 parser = argparse.ArgumentParser()
 
 # Dataset parameters
-parser.add_argument("-d", type=str, dest="dataset", default="MNIST", help="Which dataset to use (MNIST or Fashion-MNIST)")
-parser.add_argument("-s", type=int, dest="desired_image_size", default=16, help="Desired image size (shape will be size x size")
-parser.add_argument("-c", type=int, dest="n_classes_to_use", default=4, help="Number of classes to keep (only the first n classes will be kept")
-
-# Processing parameters
-parser.add_argument("--aa", action="store_true", dest="do_anti_aliasing", help="Perform anti-aliasing")
-parser.add_argument("--n", action="store_true", dest="do_normalization", help="Perform normalization")
+parser.add_argument("-d", type=str, dest="dataset", default="MNIST", help="Which dataset to use ('MNIST' or 'Fashion-MNIST')")
+parser.add_argument("-s", type=int, dest="desired_image_size", default=(16,16), help="Desired image size (as a tuple)")
+parser.add_argument("-c", type=int, dest="n_classes_to_use", default=10, help="Number of classes to keep (only the first n classes will be kept")
 
 args = parser.parse_args()
 
@@ -39,6 +35,7 @@ args = parser.parse_args()
 
 # Load the data
 (X_train, Y_train), (X_test, Y_test) = mnist.load_data() if args.dataset == "MNIST" else fashion_mnist.load_data()
+
 
 
 ##### REMOVING CLASSES #####
@@ -56,43 +53,58 @@ print(f"Training samples remaining: {n_samples_train}")
 print(f"Testing samples remaining: {n_samples_test}")
 
 
-##### RESCALING #####
 
-# Rescaling to the desired size
-def rescale_images(images):
-    desired_image_shape = (args.desired_image_size, args.desired_image_size)
-    return np.array([resize(image, desired_image_shape, anti_aliasing=args.do_anti_aliasing) for image in images])
+##### RESCALING, UNSKEWING & RESIZING #####
 
-# Rescale X_train and X_test
-X_train_small = rescale_images(X_train)
-X_test_small = rescale_images(X_test)
+# This preprocessing procedure is heavily based on the one used in WANN:
+# https://github.com/google/brain-tokyo-workshop/blob/master/WANNRelease/WANN/domain/classify_gym.py
 
+def preprocess(samples, size, n_samples, unskew=True):
+    # Create list of processed images
+    processed  = np.empty((n_samples,size[0],size[1]))
 
-##### NORMALIZATION #####
+    # Unskew and resize
+    if unskew == True:    
+        for i in range(n_samples):
+            processed[i,:,:] = deskew(cv2.resize(samples[i,:,:],size),size)
+    return processed
 
-# Normalizing data
-def normalize(images):
-    return (images - np.mean(images, axis=0))/(np.std(images, axis=0) + 0.000001)
+def deskew(image, image_shape, negated=True):
+    """ source: https://github.com/vsvinayak/mnist-helper """
+  
+    # negate the image
+    if not negated:
+        image = 255-image
+    
+    # calculate the moments of the image
+    m = cv2.moments(image)
+    if abs(m['mu02']) < 1e-2:
+        return image.copy()
+    
+    # caclulating the skew
+    skew = m['mu11']/m['mu02']
+    M = np.float32([[1, skew, -0.5*image_shape[0]*skew], [0,1,0]])
+    img = cv2.warpAffine(image, M, image_shape, flags=cv2.WARP_INVERSE_MAP|cv2.INTER_LINEAR)  
+    return img
 
-# Normalize X_train_small and X_test_small, if required
-if args.do_normalization:
-    X_train_norm = normalize(X_train_small)
-    X_test_norm = normalize(X_test_small)
+# Rescale and preprocess training and testing data
+X_train_processed = preprocess(X_train/255, args.desired_image_size, n_samples_train)
+X_test_processed = preprocess(X_test/255, args.desired_image_size, n_samples_test)
+    
     
 
 ##### WRAPPING UP #####
     
 # Replace and reshape X_train and X_test, for convenience
-X_train = (X_train_norm if args.do_normalization else X_train_small).reshape(n_samples_train, args.desired_image_size**2)
-X_test = (X_test_norm if args.do_normalization else X_test_small).reshape(n_samples_test, args.desired_image_size**2)
+X_train = X_train_processed.reshape(n_samples_train, np.prod(args.desired_image_size))
+X_test = X_test_processed.reshape(n_samples_test, np.prod(args.desired_image_size))
 
 # Check if data folder exists; if not, create it
 if not os.path.exists("data"):
     os.mkdir("data")
     
 # Set the correct filename
-filename = f"{args.dataset}-{args.n_classes_to_use}{'-Norm' if args.do_normalization else ''}" + \
-           f"{'-AA' if args.do_anti_aliasing else ''}.pkl"
+filename = f"{args.dataset}-{args.n_classes_to_use}.pkl"
 
 # Store the preprocessed data
 with open(os.path.join("data", filename), "wb") as f:
